7c9f39aab4c51b89de0c1f08f09412a777668a49
callgp <callgp@gmail.com>
2020-11-22 19:41:34 -0500

    aaa

---
+++ src/main/java/com/bharath/spring/springcore/Test.java
@@ -0,0 +1,37 @@
+/*package com.bt.spring.SpringCore;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com.bt.spring.SpringCore.config.xml");
+		Employee emp = (Employee) ctx.getBean("emp");
+		System.out.println("Employee ID: " + emp.getId());
+		System.out.println("Employee Name: " + emp.getName());
+
+	}
+
+}
+
+*/
+
+package com.bharath.spring.springcore;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bt.spring.SpringCore.Employee;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
+		Employee emp = (Employee) ctx.getBean("emp");
+		System.out.println("Employee ID: " + emp.getId());
+		System.out.println("Employee Name: " + emp.getName());
+		//test ingg
+
+	}
+
+}
